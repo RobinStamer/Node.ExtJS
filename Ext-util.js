197cb48a81fb7789cbe482701b26b619704ee6c5
Robin Stamer <Robin '|' Stamer"@xiennith.com>
2018-09-01 16:47:40 -0600

    Add Ext.pad to Ext-util.js

---
+++ Ext-util.js
@@ -0,0 +1,16 @@
+//[now.getDate(), now.getMonth(), now.getFullYear(), now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()]
+
+Ext.apply(Ext, function() {
+	return {
+		/**
+		 * Pads a string to a certain length
+		 * @param {String} str	Input value to pad 
+		 * @param {String} char	Character or other pattern to pad with
+		 * @param {Number} length	Expected output length of the final string
+		 * @return {String} Padded str
+		 */
+		pad: function pad(s, c, l) /* string s input, char c padding, num l length */ {
+			return (c.repeat(l) + s).substr(-l)
+		}
+	}
+}
