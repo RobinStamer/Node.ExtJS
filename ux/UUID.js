cc31c2bdcda1a155fa88fcdd3eaf41e0155a28b4
Mateusz Naściszewski <matin1111@wp.pl>
2021-11-10 17:29:52 +0100

    Document Ext.ux.UUID in a hacky manner

--- ux/UUID.js
+++ ux/UUID.js
@@ -1,5 +1,22 @@
 Ext.ns('Ext.ux')
 
+/**
+ * @class Ext.ux.UUID
+ *
+ * An easily accessible version of the node uuid library.
+ */
+
+/**
+ * Creates a v4 (random) UUID.
+ *
+ * @method UUID
+ * @return {String} random UUID
+ */
 Ext.ux.UUID = Ext.apply(function() {
 	return Ext.ux.UUID.v4()
 }, require('uuid'))
+
+// Work around a documentation generator bug, otherwise MsgBus.js leaks into this file
+/**
+ * @class
+ */
