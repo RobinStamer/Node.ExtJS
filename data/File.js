fc9179bf5c6d4b4bc49515a701a273607c184555
Mateusz Naściszewski <matin1111@wp.pl>
2021-11-10 16:24:02 +0100

    Document Ext.data.File

--- data/File.js
+++ data/File.js
@@ -3,7 +3,23 @@ const	Ext	= require('Ext')('Ext.class')
 	
 Ext.ns('Ext.data')
 
+/**
+ * @class Ext.data.File
+ * @extends fs.ReadStream
+ *
+ * Streams a file from the filesystem.
+ *
+ * @constructor
+ * @param {Object} cfg
+ */
 class File extends fs.ReadStream {
+	/**
+	 * @cfg {String} name File path to read
+	 */
+
+	/**
+	 * @cfg {String} encoding Encoding to use for reading the file, defaults to utf8
+	 */
 	constructor(cfg) {
 		super(cfg.name, {
 			encoding: cfg.encoding || 'utf8'
