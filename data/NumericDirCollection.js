1cf7359a113840e40390b7d9b20a9b4925302666
Mateusz Naściszewski <matin1111@wp.pl>
2021-11-10 16:48:14 +0100

    Document Ext.data.NumericDirCollection, which is currently broken

--- data/NumericDirCollection.js
+++ data/NumericDirCollection.js
@@ -1,5 +1,18 @@
 Ext('Ext.data.DirCollection')
 
+/**
+ * @class Ext.data.NumericDirCollection
+ * @extends Ext.data.DirCollection
+ *
+ * DirCollection with numeric keys.
+ * WARNING: Currently broken, don't use!
+ *
+ * @constructor
+ * @param {Object} cfg A config object
+ *
+ * @xtype numdircol
+ */
+
 class NumericDirCollection extends Ext.data.DirCollection {
 	constructor(cfg) {
 		super(Ext.apply({
