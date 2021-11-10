72573cb479b9a742575909bd61cd8afeabbc57b7
Mateusz Naściszewski <matin1111@wp.pl>
2021-11-10 17:10:58 +0100

    Document loose Ext.fs functions (as methods on Ext singleton)

--- fs/index.js
+++ fs/index.js
@@ -5,10 +5,22 @@ Ext.ns('Ext.fs')
 
 // Original: https://github.com/substack/node-mkdirp (Mit/X11)
 
+/**
+ * @class Ext
+ */
+
+/**
+ * Creates a directory and all necessary parent directories, asynchronously.
+ *
+ * @method fs.mkdirP
+ * @param {String} p Path to create
+ * @param {Number} mode Octal mode mask
+ * @param {Function} f Callback, called with error on error, null or undefined on success
+ */
 Ext.fs.mkdirP = function(p, mode, f) {
     var cb = f || function () {};
     p = path.resolve(p);
-    
+
     var ps = path.normalize(p).split('/');
     fs.exists(p, function (exists) {
         if (exists) cb(null)
@@ -22,10 +34,26 @@ Ext.fs.mkdirP = function(p, mode, f) {
     })
 }
 
+/**
+ * Loads a JSON-encoded object from a specified file.
+ *
+ * @method fs.loadJSON
+ * @param {String} filename Path to load the file from
+ * @param {String} enc File encoding to use
+ * @return {Any}
+ */
 Ext.fs.loadJSON = function(filename, enc) {
 	return JSON.parse(fs.readFileSync((Ext.path || function(f) { return f })(filename), enc).toString())
 }
 
+/**
+ * Writes an object to a given file, as JSON.
+ *
+ * @method fs.saveJSON
+ * @param {String} filename Path to save the file to
+ * @param {Any} data Data to encode as JSON
+ * @param {String} enc File encoding to use
+ */
 Ext.fs.saveJSON = function(filename, data, enc) {
 	return fs.writeFileSync((Ext.path || function(f) { return f })(filename), JSON.stringify(data), enc)
 }
