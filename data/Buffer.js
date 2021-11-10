f79c934df39523681f059de5766468ad34009e60
Mateusz Naściszewski <matin1111@wp.pl>
2021-11-10 15:55:40 +0100

    Document Ext.data.Buffer

--- data/Buffer.js
+++ data/Buffer.js
@@ -2,16 +2,37 @@ var Ext = require('Ext')
 
 Ext.ns('Ext.data')
 
+/**
+ * @class Ext.data.Buffer
+ * Accepts Buffer data from a stream and converts it to String.
+ *
+ * @constructor
+ * @param {Stream?} o Object to attach data listeners to
+ */
 class Buffer {
 	constructor(o) {
 		if (o) {
 			this.apply(o)
 		}
 
+		/**
+		 * String encoding to use when reading buffers.
+		 * @property
+		 * @type String
+		 */
 		this.encoding = 'utf8'
+		/**
+		 * Data which was read from the stream.
+		 * @property
+		 * @type String
+		 */
 		this.buffer = ''
 	}
 
+	/**
+	 * Attaches itself to data listeners on the given object.
+	 * @param {Stream} o Object on which to listen for data
+	 */
 	apply(o) {
 		const self = this
 
@@ -19,10 +40,19 @@ class Buffer {
 		o.on('end', function() { self.hook(self.buffer) })
 	}
 
+	/**
+	 * Appends data to internal buffer. May be used manually if a stream is not attached.
+	 * @param {Buffer} data
+	 */
 	input(data) {
 		this.buffer += data.toString(this.encoding)
 	}
 
+	/**
+	 * Called after fully reading the data.
+	 * You may override this with a callback.
+	 * @param {String} data
+	 */
 	hook() {
 		return this.buffer
 	}
