a4039fac946474beeb2fb8e6f0b53b9a816c8289
Mateusz Naściszewski <matin1111@wp.pl>
2021-11-11 16:08:35 +0100

    Document what can be documented of Ext.net.LineSocket

--- net/LineSocket.js
+++ net/LineSocket.js
@@ -19,15 +19,35 @@ function onBind(self) {
 	})
 
 	if (self.config.client) this.on('close', function() {
+		// FIXME: No such event is defined on LineSocket
 		self.fireEvent('disconnect')
 		delete self._socket
 		self.connect()
 	})
 
+	// FIXME: No such event is defined on LineSocket
 	self.fireEvent('connect')
 }
 
+/**
+ * @class Ext.net.LineSocket
+ * @extends Ext.util.Observable
+ * Implements a client and server for line-based protocols.
+ *
+ * @constructor
+ * @param {Object} config
+ */
+
 Ext.net.LineSocket = Ext.extend(Ext.util.Observable, {
+	/**
+	 * @cfg {?} listeners (seems unused)
+	 */
+	/**
+	 * @cfg {Object} server {host, port} pair to listen on, overrides {@link #client}
+	 */
+	/**
+	 * @cfg {Object} client {host, port} pair to connect to
+	 */
 	constructor: function(config) {
 		var self = this
 		this.config = config
@@ -40,9 +60,28 @@ Ext.net.LineSocket = Ext.extend(Ext.util.Observable, {
 		this.connect()
 
 		this.addEvents(
+			/**
+			 * @event line
+			 * Fires on every received line
+			 * @param {String} line Received line, without line terminators
+			 * @param {Socket} this FIXME: Is this the LineSocket, our net.Socket, or the client net.Socket?
+			 */
 			'line',
+			/**
+			 * @event reconnect
+			 * FIXME: Is this used?
+			 */
 			'reconnect',
+			/**
+			 * @event client
+			 * Fires when a client connects to this socket.
+			 * @param {Socket} client
+			 */
 			'client',
+			/**
+			 * @event end
+			 * FIXME: Is this used? Socket events aren't forwarded to this class.
+			 */
 			'end'
 		)
 
@@ -53,12 +92,24 @@ Ext.net.LineSocket = Ext.extend(Ext.util.Observable, {
 
 		Ext.net.LineSocket.superclass.constructor.call(this)
 	},
+	/**
+	 * Returns whether the socket is in ready state.
+	 * @return {Boolean}
+	 */
 	ready: function() {
 		return 'open' == this._socket.readyState
 	},
+	/**
+	 * Writes data directly into the socket.
+	 * @param {String|Buffer} d
+	 */
 	send: function(d) {
 		this._socket.write(d);
 	},
+	/**
+	 * Connects or listens depending on configured parameters
+	 * @method
+	 */
 	connect: function() {
 		if (this.config.server) {
 			this._socket = net.createServer()
