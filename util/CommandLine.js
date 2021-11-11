21e00a57ff1cd0fe14327bbc3f3377b6b299e563
Mateusz Naściszewski <matin1111@wp.pl>
2021-11-11 14:50:32 +0100

    Document Ext.util.CommandLine, which seems fairly broken

--- util/CommandLine.js
+++ util/CommandLine.js
@@ -1,9 +1,32 @@
+/**
+ * @class Ext.util.CommandLine
+ * Processes command-line arguments
+ *
+ * @constructor
+ * @param {Object} options
+ */
 Ext.util.CommandLine = Ext.extend(function(options) {
 	Ext.apply(this, options)
 }, {
+	/**
+	 * @cfg {Function} func
+	 * Function to call when {@link #run} is called
+	 */
+	/**
+	 * Parses a line of command input. Does not handle strings or escaping.
+	 * FIXME: Broken, returns leftover positional args, not parsed opts!
+	 * @param {String} line
+	 * @return {???}
+	 */
 	parseLine: function(line) {
 		return this.parseWords(line.split(/ /g))
 	},
+	/**
+	 * Parses an array of command line arguments.
+	 * FIXME: Broken, returns leftover positional args, not parsed opts, it just throws those away!
+	 * @param {String[]} words
+	 * @return {???}
+	 */
 	parseWords: function(words) {
 		var opts = {}
 
@@ -26,7 +49,14 @@ Ext.util.CommandLine = Ext.extend(function(options) {
 
 		return words
 	},
+	/**
+	 * Runs the object's func handler with parsed arguments from o.argv or o.arg.
+	 * @param {Object} o
+	 * @return {Any}
+	 * Returns whatever the handler returns.
+	 */
 	run: function(o) {
+		// FIXME: calls parse methods with two arguments but they only accept one
 		var arg = (o.argv) ? this.parseWords(o.argv, o.obj) : this.parseLine(o.arg, o.obj);
 
 		return this.func.call(o.scope || this, arg)
