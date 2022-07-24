b9e11a257cc15a6d2b573796c31fffb6ea04dc06
R <R@nobl>
2022-07-24 03:49:46 -0600

    Add Ext.data.GamePipe to read linux joystick input

---
+++ data/GamePipe.js
@@ -0,0 +1,58 @@
+var	fs	= require('fs')
+
+Ext('Ext.data.Pipe')
+
+class GamePipe extends Ext.data.Pipe {
+	constructor(cfg) {
+		if ('number' == typeof cfg.input) {
+			cfg.input = `/dev/input/js${cfg.input}`
+		}
+
+		if ('string' == typeof cfg.input) {
+			// Use this instead of xtype file so input isn't duplicated
+			cfg.input = new fs.ReadStream(cfg.input)
+		}
+
+		super(cfg)
+
+		if (cfg.input) {
+			Ext.data.Pipe.pipe(this)
+		}
+	}
+
+	_transform(chunk, enc, done) {
+		var b = this.hold ? Buffer.concat([this.hold, chunk]) : chunk
+
+		console.log(b.length)
+
+		delete this.hold
+
+		while (b.length >= 8) {
+			var ev = {
+				// u32 Time ; s16 value ; u8 type ; u8 number
+				number:	b.readUInt8(7)
+				,time:	b.readUInt32LE(0)
+				,value:	b.readInt16LE(4)
+				,type:	b.readUInt8(6)
+			}
+
+			ev.isButton	= !!(ev.type & 0x01)
+			ev.isAxis	= !!(ev.type & 0x02)
+			ev.isInit	= !!(ev.type & 0x80)
+
+			ev.b = b.slice(0, 8)
+
+			this.push(ev)
+
+			b = b.slice(8)
+		}
+
+		this.hold = b
+
+		done()
+	}
+}
+
+Ext.data.GamePipe = GamePipe
+
+Ext.reg('gamepipe', Ext.data.GamePipe)
--- data/Pipe.js
+++ data/Pipe.js
@@ -2,13 +2,22 @@ var stream = require('stream')
 
 class Pipe extends stream.Transform {
 	constructor(cfg) {
-		super(Ext.apply(cfg.cfg || {}, {readableObjectMode: true}))
+		Ext.applyIf(cfg, {cfg: {}})
+
+		super(Ext.apply(cfg.cfg, {readableObjectMode: true}))
 
 		this.cfg = cfg
 	}
 
 	static pipe(o) {
 		if (o.cfg.input) {
+			if ('string' == typeof o.cfg.input) {
+				o.cfg.input = {
+					xtype:	'file'
+					,name:	o.cfg.input
+				}
+			}
+
 			if (o.cfg.input.xtype && !o.cfg.input.pipe) {
 				o.cfg.input = Ext.xcreate(o.cfg.input)
 			}
