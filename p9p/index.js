a17df9747f32a61d9cb03683036c26a816391793
R <R@nobl>
2022-08-01 11:50:38 -0600

    Add p9p.getNS() function

--- p9p/index.js
+++ p9p/index.js
@@ -16,3 +16,17 @@ Ext.p9p.plumbListen = function(port) {
 		}
 	})
 }
+
+Ext.p9p.getNS = function() {
+	var ns = process.env.NAMESPACE || this.getNSfromDisplay()
+
+	return new Promise((p,f) => {
+		p(ns)
+	})
+}
+
+Ext.p9p.getNSfromDisplay = function() {
+	var os = Ext.lib.os || require('os'), display = process.env.DISPLAY || ':0'
+
+	return `/tmp/ns.${os.userInfo().username}.${display}`
+}
