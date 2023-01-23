436272fdc8a27761525d6dbd604c3ce9c60f4fad
R <R@nobl>
2023-01-22 22:58:42 -0700

    Fix doc strings in Ext.fs

--- fs/index.js
+++ fs/index.js
@@ -6,13 +6,14 @@ Ext.ns('Ext.fs')
 // Original: https://github.com/substack/node-mkdirp (Mit/X11)
 
 /**
- * @class Ext
+ * @class Ext.fs
+ * @singleton
  */
 
 /**
  * Creates a directory and all necessary parent directories, asynchronously.
  *
- * @method fs.mkdirP
+ * @method mkdirP
  * @param {String} p Path to create
  * @param {Number} mode Octal mode mask
  * @param {Function} f Callback, called with error on error, null or undefined on success
@@ -37,7 +38,7 @@ Ext.fs.mkdirP = function(p, mode, f) {
 /**
  * Loads a JSON-encoded object from a specified file.
  *
- * @method fs.loadJSON
+ * @method loadJSON
  * @param {String} filename Path to load the file from
  * @param {String} enc File encoding to use
  * @return {Any}
@@ -49,7 +50,7 @@ Ext.fs.loadJSON = function(filename, enc) {
 /**
  * Writes an object to a given file, as JSON.
  *
- * @method fs.saveJSON
+ * @method saveJSON
  * @param {String} filename Path to save the file to
  * @param {Any} data Data to encode as JSON
  * @param {String} enc File encoding to use
