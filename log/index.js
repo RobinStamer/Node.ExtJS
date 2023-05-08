3e37bf6c8ab0972e33f02a555f23f273d1b555fb
R <R@nobl>
2023-05-07 19:27:54 -0600

    Add list of docblock tags

--- log/index.js
+++ log/index.js
@@ -40,7 +40,7 @@ class Log {
 	/**
 	 * Change the default log level
 	 * @param {Number|String} l The log level to set.  Eg: <code>'PRODUCTION'</code>, <code>'DEVELOPMENT'</code>, <code>Ext.log.FATAL | Ext.log.ERROR</code>
-	 * @returns {Number} The value set
+	 * @return {Number} The value set
 	 */
 	level(l) {
 		return this._level = 'string' == typeof l ? this[l.toUpperCase()] : l
@@ -51,7 +51,7 @@ class Log {
 	 * @param {Number} type Message type, eg: Ext.log.FATAL
 	 * @param {String} src Message source, eg: 'lighttp'
 	 * @param {String} txt Message to log
-	 * @returns {Any} Value from this.dolog or `null` if dolog() was not called
+	 * @return {Any} Value from this.dolog or `null` if dolog() was not called
 	 */
 	log(type, src, txt) {
 		if (type & this._level) {
@@ -74,7 +74,7 @@ class Log {
 	 * Shortcut function to log a fatal error
 	 * @param {String} src Message source
 	 * @param {String} txt Message body
-	 * @returns {Any} Return value from {@link Ext.log.log}
+	 * @return {Any} Return value from {@link Ext.log.log}
 	 */
 	fatal(src, txt) {
 		return this.log(this.FATAL, src, txt)
@@ -84,7 +84,7 @@ class Log {
 	 * Shortcut function to log a standard error
 	 * @param {String} src Message source
 	 * @param {String} txt Message body
-	 * @returns {Any} Return value from {@link Ext.log.log}
+	 * @return {Any} Return value from {@link Ext.log.log}
 	 */
 	error(src, txt) {
 		return this.log(this.ERROR, src, txt)
@@ -94,7 +94,7 @@ class Log {
 	 * Shortcut function to log a warning
 	 * @param {String} src Message source
 	 * @param {String} txt Message body
-	 * @returns {Any} Return value from {@link Ext.log.log}
+	 * @return {Any} Return value from {@link Ext.log.log}
 	 */
 	warn(src, txt) {
 		return this.log(this.WARN, src, txt)
@@ -104,7 +104,7 @@ class Log {
 	 * Shortcut function to log an informational message
 	 * @param {String} src Message source
 	 * @param {String} txt Message body
-	 * @returns {Any} Return value from {@link Ext.log.log}
+	 * @return {Any} Return value from {@link Ext.log.log}
 	 */
 	info(src, txt) {
 		return this.log(this.INFO, src, txt)
@@ -114,7 +114,7 @@ class Log {
 	 * Shortcut function to log a debug message
 	 * @param {String} src Message source
 	 * @param {String} txt Message body
-	 * @returns {Any} Return value from {@link Ext.log.log}
+	 * @return {Any} Return value from {@link Ext.log.log}
 	 */
 	debug(src, txt) {
 		return this.log(this.DEBUG, src, txt)
